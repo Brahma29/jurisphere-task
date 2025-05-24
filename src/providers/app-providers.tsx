2c7edf92d99560a8c939ef54a9eeb19c85a9428b
Brahmanand Swami <brahmanandswami15@gmail.com>
2025-05-24 20:29:44 +0530

    feat: wrap children in Suspense

--- src/providers/app-providers.tsx
+++ src/providers/app-providers.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { type ReactNode } from "react";
+import { Suspense, type ReactNode } from "react";
 
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
@@ -11,7 +11,7 @@ import { Toaster } from "sonner";
 const AppProviders = ({ children }: { children: ReactNode }) => {
   return (
     <QueryClientProvider client={queryClient}>
-      {children}
+      <Suspense>{children}</Suspense>
       <Toaster position="top-center" duration={1000} />
       <ReactQueryDevtools buttonPosition="bottom-left" initialIsOpen={false} />
     </QueryClientProvider>
