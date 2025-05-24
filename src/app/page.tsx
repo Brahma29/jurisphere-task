e8069417aa5f79e5f3ca663d8a3b62225e040f29
Brahmanand Swami <brahmanandswami15@gmail.com>
2025-05-24 17:55:47 +0530

    feat: implement column sorting and filtering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,125 @@
+"use client";
+
 import { data } from "@/constants/dummay-data";
-import React from "react";
+import React, { useMemo, useState } from "react";
+
+type TSortingDirection = "asc" | "desc";
+type TUserStatus = "active" | "inactive";
+
+type TCurrentSortConfig = {
+  key: keyof TUser | null;
+  direction: TSortingDirection;
+};
+
+export type TUser = {
+  id: number;
+  name: string;
+  email: string;
+  status: TUserStatus;
+};
 
 export default function Home() {
+  const [currentSortConfig, setCurrentSortConfig] =
+    useState<TCurrentSortConfig>({
+      key: null,
+      direction: "asc",
+    });
+  const [nameFilter, setNameFilter] = useState<string>("");
+  const [statusFilter, setStatusFilter] = useState<TUserStatus | null>(null);
+
+  const handleSort = (key: TCurrentSortConfig["key"]) => {
+    let direction: TSortingDirection = "asc";
+    if (
+      currentSortConfig.key === key &&
+      currentSortConfig.direction === "asc"
+    ) {
+      direction = "desc";
+    }
+    setCurrentSortConfig({ key, direction });
+  };
+
+  const sortData = (data: TUser[]) => {
+    if (!currentSortConfig.key) return data;
+
+    return [...data].sort((a, b) => {
+      const aValue = a[currentSortConfig.key!];
+      const bValue = b[currentSortConfig.key!];
+
+      if (aValue < bValue) {
+        return currentSortConfig.direction === "asc" ? -1 : 1;
+      }
+      if (aValue > bValue) {
+        return currentSortConfig.direction === "asc" ? 1 : -1;
+      }
+      return 0;
+    });
+  };
+
+  const filterData = (data: TUser[]) => {
+    return data.filter((item) => {
+      const nameMatch = item.name
+        .toLowerCase()
+        .includes(nameFilter.toLowerCase());
+      const statusMatch = statusFilter === null || item.status === statusFilter;
+      return nameMatch && statusMatch;
+    });
+  };
+
+  const filteredData = useMemo(
+    () => filterData(data),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [data, nameFilter, statusFilter]
+  );
+
+  const filteredAndSortedData = useMemo(
+    () => sortData(filteredData),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [filteredData, currentSortConfig]
+  );
+
   return (
     <div className="max-w-2xl mx-auto py-10">
-      <h1 className="text-2xl font-bold">Users</h1>
+      <h1 className="text-2xl font-bold">Manage Users</h1>
+
+      <div className="my-4 flex gap-4">
+        <input
+          id="nameFilter"
+          type="text"
+          placeholder="Search by name..."
+          value={nameFilter}
+          onChange={(e) => setNameFilter(e.target.value)}
+          className="w-full px-3 py-1 border border-gray-300 rounded-md focus:outline-none "
+        />
+
+        <select
+          id="statusFilter"
+          value={statusFilter || ""}
+          onChange={(e) =>
+            setStatusFilter(
+              e.target.value === "" ? null : (e.target.value as TUserStatus)
+            )
+          }
+          className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none flex-1 min-w-36"
+        >
+          <option value="">All Status</option>
+          <option value="active">Active</option>
+          <option value="inactive">Inactive</option>
+        </select>
+      </div>
 
       <div>
         <table className="w-full">
           <thead>
             <tr>
-              <TableHead>id</TableHead>
-              <TableHead>name</TableHead>
-              <TableHead>email</TableHead>
-              <TableHead>status</TableHead>
+              <TableHead onClick={() => handleSort("id")}>id</TableHead>
+              <TableHead onClick={() => handleSort("name")}>name</TableHead>
+              <TableHead onClick={() => handleSort("email")}>email</TableHead>
+              <TableHead onClick={() => handleSort("status")}>status</TableHead>
             </tr>
           </thead>
 
           <tbody>
-            {data.map((user) => (
+            {filteredAndSortedData.map((user) => (
               <tr key={user.id}>
                 <TableData>{user.id}</TableData>
                 <TableData>{user.name}</TableData>
@@ -33,8 +134,16 @@ export default function Home() {
   );
 }
 
-const TableHead = ({ children }: { children: React.ReactNode }) => (
-  <th className="text-left py-2 bg-gray-400">{children}</th>
+const TableHead = ({
+  children,
+  onClick,
+}: {
+  children: React.ReactNode;
+  onClick?: () => void;
+}) => (
+  <th className="text-left py-2 bg-gray-400" onClick={onClick}>
+    {children}
+  </th>
 );
 
 const TableData = ({ children }: { children: React.ReactNode }) => (
--- src/constants/dummay-data.ts
+++ src/constants/dummay-data.ts
@@ -1,57 +1,59 @@
-export const data = [
-  { id: 1, name: "John Doe", email: "john.doe@example.com", status: "Active" },
+import { TUser } from "@/app/page";
+
+export const data: TUser[] = [
+  { id: 1, name: "John Doe", email: "john.doe@example.com", status: "active" },
   {
     id: 2,
     name: "Jane Smith",
     email: "jane.smith@example.com",
-    status: "Inactive",
+    status: "inactive",
   },
   {
     id: 3,
     name: "Mike Johnson",
     email: "mike.johnson@example.com",
-    status: "Active",
+    status: "active",
   },
   {
     id: 4,
     name: "Sarah Wilson",
     email: "sarah.wilson@example.com",
-    status: "Pending",
+    status: "inactive",
   },
   {
     id: 5,
     name: "David Brown",
     email: "david.brown@example.com",
-    status: "Active",
+    status: "active",
   },
   {
     id: 6,
     name: "Lisa Davis",
     email: "lisa.davis@example.com",
-    status: "Inactive",
+    status: "inactive",
   },
   {
     id: 7,
     name: "Tom Anderson",
     email: "tom.anderson@example.com",
-    status: "Pending",
+    status: "active",
   },
   {
     id: 8,
     name: "Emma Taylor",
     email: "emma.taylor@example.com",
-    status: "Active",
+    status: "active",
   },
   {
     id: 9,
     name: "Chris Miller",
     email: "chris.miller@example.com",
-    status: "Inactive",
+    status: "inactive",
   },
   {
     id: 10,
     name: "Anna White",
     email: "anna.white@example.com",
-    status: "Active",
+    status: "active",
   },
 ];
